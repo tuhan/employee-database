afda991308816118259994c29fc8191125f442a8
Tuhan Sapumanage <tuhan.binuja@gmail.com>
2019-03-14 18:31:37 +0530

    commit One

---
+++ Employee Database/Employee.swift
@@ -0,0 +1,9 @@
+//
+//  Employee.swift
+//  Employee Database
+//
+//  Created by Tuhan Sapumanage on 3/14/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import Foundation
---
+++ Employee Database/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Employee Database
+//
+//  Created by Tuhan Sapumanage on 3/14/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
